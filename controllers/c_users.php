50906ae2be55daffcca83f32d33b94d9d4a12791
djrunner <danielbreidenbach@gmail.com>
2013-10-20 20:04:11 -0400

    edited profile function and created upload_image function. Also added edited the logout function

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -118,20 +118,98 @@ class users_controller extends base_controller {
     }
 
     public function logout() {
-        echo "This is the logout page";
+        # Generate and save a new token for the next Login
+        $new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
+
+        # Create the data array we'll use with the update method
+        # In this case, we're only updating one field, so our array only has one entry
+        $data = Array("token" => $new_token);
+
+        # Do the update
+        DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
+
+        # Delete their token cookie by setting it to a date in the past - logging them out
+        setcookie("token", "", strtotime('-1 year'), '/');
+
+        #Send them back to the main index
+        Router::redirect("/");
+
 
     }
 
-    public function profile($user_name = NULL) {
+    public function profile($user_name = NULL, $error = NULL) {
 
+        #If user is blank, they're not logged in; redirect them to the Login page
+        if (!$this->user) {
+            Router::redirect('/users/login');
+        }
+
+        # If they weren't redirected away, continue:
+
+        # Setup view
         $this->template->content = View::instance('v_users_profile');
-        $this->template->title = "Profile";
+        $this->template->title = "Profile of ".$this->user->first_name;
+
+        # Set up error
+        $this->template->content->error = $error;
 
         $this->template->content->user_name = $user_name;
+
+        #Create array of CSS files
+        $client_files_head = Array (
+            '../css/profile.css',
+            '../../css/profile.css',
+            '../../css/error.css'
+            );
+
+        #Use Load client_files to generate the links from the above array
+        $this->template->client_files_head = Utils::load_client_files($client_files_head);
 
         echo $this->template;
 
         
     }
 
+    public function upload_image() {
+
+        $allowedExts = array("gif", "jpeg", "jpg", "png");
+        $temp = explode(".", $_FILES["file"]["name"]);
+        $extension = end($temp);
+        if ((($_FILES["file"]["type"] == "image/gif")
+        || ($_FILES["file"]["type"] == "image/jpeg")
+        || ($_FILES["file"]["type"] == "image/jpg")
+        || ($_FILES["file"]["type"] == "image/pjpeg")
+        || ($_FILES["file"]["type"] == "image/x-png")
+        || ($_FILES["file"]["type"] == "image/png"))
+        && ($_FILES["file"]["size"] < 20000)
+        && in_array($extension, $allowedExts)) {
+
+            if ($_FILES["file"]["error"] > 0) {
+                echo "Error: " . $_FILES["file"]["error"] . "<br>";
+            }
+
+            else {
+    
+                if (file_exists("images/" . $_FILES["file"]["name"])) {
+                    echo $_FILES["file"]["name"] . " already exists. ";
+
+                    Router::redirect("/users/profile");
+                } 
+                else {
+                    move_uploaded_file($_FILES["file"]["tmp_name"],
+                    "images/" . $_FILES["file"]["name"]);
+                    echo "Stored in: " . "images/" . $_FILES["file"]["name"];
+
+
+                    $data = Array("image_location" => "images/" . $_FILES["file"]["name"]);
+                    DB::instance(DB_NAME)->update("users", $data, "WHERE user_id = '".$this->user->user_id."'");
+
+                    Router::redirect('/users/profile');
+                }
+            }
+        }
+        else {
+            Router::redirect("/users/profile/error");
+        }
+    }
 } # end of the class
