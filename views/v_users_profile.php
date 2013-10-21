99949045958d5b0128dc1c2c55e38f03bfeac3b8
djrunner <danielbreidenbach@gmail.com>
2013-10-20 20:06:51 -0400

    crated view for user profile, including image upage option

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,5 +1,19 @@
-<?php if(isset($user_name)): ?>
-    <h1>This is the profile for <?=$user_name?></h1>
-<?php else: ?>
-    <h1>No user specified</h1>
-<?php endif; ?>
+<div id="profile">
+
+<h1>This is the profile of <?=$user->first_name?></h1>
+
+<h3>E-mail Address: <?=$user->email?></h3>
+
+<p>
+	Picture!</br>
+	
+	<form method="POST" action="/users/upload_image" enctype="multipart/form-data">
+		<label for="file">Filename:</label>
+		<input type="file" name="file" id="file"><br>
+		<input type="submit" name="submit" value="Submit">
+	</form>
+
+	<img src="../<?=$user->image_location?>">
+</p>
+
+</div>
