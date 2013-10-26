012e2e06c06201ae5270eeb57ef3f10312404809
djrunner <danielbreidenbach@gmail.com>
2013-10-26 17:51:31 -0400

    added menu bar

--- views/_v_template.php
+++ views/_v_template.php
@@ -8,9 +8,33 @@
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
-</head>
+</head> 
 
 <body>	
+
+	<div id='menu'>
+
+		<a href='/'>Home</a>
+
+		<!-- Menu for users who ar logged in -->
+		<?php if($user): ?>
+
+			<a href='/users/logout'>Logout</a>
+			<a href='/users/Profile'>Profile</a>
+			<a href='/posts/index'>Posts</a>
+			<a href='/posts/users'>Users</a>
+
+		<!-- Menu options for users who are not logged in -->
+		<?php else: ?>
+
+            <a href='/users/signup'>Sign up</a>
+            <a href='/users/login'>Log in</a>
+
+        <?php endif; ?>
+
+    </div>
+
+    <br>
 
 	<?php if(isset($content)) echo $content; ?>
 
